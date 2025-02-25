9e3a13dbede2d5923e8d6fe106b72d6f89755508
PlusNguyn <tranthainguyenpntlop12c8@gmail.com>
2025-02-25 16:36:09 +0700

    first uploadupload

---
+++ blogapp/blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import PostModel
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'updated_at']
+
+admin.site.register(PostModel, PostModelAdmin)
+
+# Register your models here.
